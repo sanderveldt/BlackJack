28d0d0f22470618ccdc500fb92fb99eb8d7d91e9
Sander <sanderveldt98@gmail.com>
2026-05-30 20:27:19 +0200

    gui

--- GUI.py
+++ GUI.py
@@ -2,14 +2,36 @@ import customtkinter as ctk
 
 window = ctk.CTk()
 
-window.geometry("800x800")
+window.geometry("800x600")
 window.title("Blackjack")
+
+window.grid_rowconfigure(0, weight=2)
+window.grid_rowconfigure(1, weight=1)
+window.grid_rowconfigure(2, weight=2)
+window.grid_rowconfigure(3, weight=1)
+
+window.grid_columnconfigure(0, weight=1)
+
+dealer_frame = ctk.CTkFrame(window)
+
+
+info_frame = ctk.CTkFrame(window)
+
+
+player_frame = ctk.CTkFrame(window)
+
+
+button_frame = ctk.CTkFrame(window)
+
+button_frame.grid_columnconfigure(0, weight=1)
+button_frame.grid_columnconfigure(1, weight=1)
+
 
 def hit():
     print('You hit.')
 
 hit_button = ctk.CTkButton(
-    window,
+    button_frame,
     text = "Hit",
     command = hit
 )
@@ -18,17 +40,21 @@ def stand():
     print("You stand.")
 
 stand_button = ctk.CTkButton(
-    window,
+    button_frame,
     text = "Stand",
-    command = stand
-    
+    command = stand   
 )
 
-hit_button.pack(padx=10,pady=10)
-stand_button.pack(padx=100,pady=10)
 
-title = ctk.CTkLabel(window, text = "Blackjack")
-title.pack(pady=100)
+
+dealer_frame.grid(row=0, column=0, sticky="nsew")
+info_frame.grid(row=1, column=0, sticky="nsew")
+player_frame.grid(row=2, column=0, sticky="nsew")
+button_frame.grid(row=3, column=0, sticky="nsew")
+hit_button.grid(row=3, column=0, sticky="e")
+stand_button.grid(row=3, column=1, sticky="w")
+
+
 
 window.mainloop()
 
