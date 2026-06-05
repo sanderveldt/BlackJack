069e564ba6705ffe93e84624e6925329c051734a
Sander <sanderveldt98@gmail.com>
2026-06-05 22:56:27 +0200

    setup screen defined

--- GUI.py
+++ GUI.py
@@ -1,32 +1,53 @@
 import customtkinter as ctk
+import Blackjack_gui as bjg
 
+### Set-up
 window = ctk.CTk()
-
 window.geometry("800x600")
 window.title("Blackjack")
+
+def setup_screen():
+    setup_frame = ctk.CTkFrame(window)
+    setup_frame.place(relx=0.5, rely=0.5, anchor="center")
+    
+    setup_frame_label = ctk.CTkLabel(setup_frame, text="Welcome to Blackjack!")
+    setup_name_label = ctk.CTkLabel(setup_frame, text="Player Name:")
+    setup_name_entry = ctk.CTkEntry(setup_frame, placeholder_text="Enter your name")    
+    setup_balance_label = ctk.CTkLabel(setup_frame, text="Starting balance:")
+    setup_balance_entry = ctk.CTkEntry(setup_frame, placeholder_text="Enter starting balance")
+    setup_status_label = ctk.CTkLabel(setup_frame, text="")
+    setup_start_button = ctk.CTkButton(setup_frame, text="Start Game")
+
+    setup_frame_label.grid(row=0, column=0, columnspan=2)
+    setup_name_label.grid(row=1, column=0)
+    setup_name_entry.grid(row=1, column=1)
+    setup_balance_label.grid(row=2, column=0)
+    setup_balance_entry.grid(row=2, column=1)
+    setup_status_label.grid(row=3, column=0, columnspan=2)
+    setup_start_button.grid(row=4, column=0, columnspan=2)
+
 
 window.grid_rowconfigure(0, weight=2)
 window.grid_rowconfigure(1, weight=1)
 window.grid_rowconfigure(2, weight=2)
 window.grid_rowconfigure(3, weight=1)
-
 window.grid_columnconfigure(0, weight=1)
 
+### Frames
 dealer_frame = ctk.CTkFrame(window)
-
 dealer_frame.grid_columnconfigure(0, weight=1)
 
 dealer_title = ctk.CTkLabel(dealer_frame, text="Dealer")
-
+dealer_hand = ctk.CTkTextbox(dealer_frame)
 
 info_frame = ctk.CTkFrame(window)
 
 
 player_frame = ctk.CTkFrame(window)
 
+player_textbox = ctk.CTkTextbox(player_frame)
 
 button_frame = ctk.CTkFrame(window)
-
 button_frame.grid_columnconfigure(0, weight=1)
 button_frame.grid_columnconfigure(1, weight=1)
 
@@ -43,8 +64,11 @@ stand_button = ctk.CTkButton(button_frame, text = "Stand", command = stand)
 
 dealer_frame.grid(row=0, column=0, sticky="nsew")
 dealer_title.grid(row=0, column=0, stick="n")
+
 info_frame.grid(row=1, column=0, sticky="nsew")
+
 player_frame.grid(row=2, column=0, sticky="nsew")
+
 button_frame.grid(row=3, column=0, sticky="nsew")
 hit_button.grid(row=3, column=0, sticky="e")
 stand_button.grid(row=3, column=1, sticky="w")
